0a10c0fbeac788d2650461434b3fc03052acf52b
Ayush Nighoskar <ayunighoskar@gmail.com>
2023-04-16 14:03:34 +0530

    Added js and json files
    Added js and json files

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css'  //npm i bootstrap-dark-5 boostrap
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+
+import Home from './screens/Home';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+// import Navbar from './components/Navbar';
+import Login from './screens/Login';
+import Signup from './screens/Signup';
+import { CartProvider } from './components/ContextReducer';
+import MyOrder from './screens/MyOrder';
+
+
+function App() {
+  return (
+    <CartProvider>
+      <Router>
+        <div>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/signup" element={<Signup />} />
+            <Route exact path="/myorder" element={<MyOrder />} />
+          </Routes>
+        </div>
+      </Router>
+    </CartProvider>
+  );
+}
+
+export default App;
